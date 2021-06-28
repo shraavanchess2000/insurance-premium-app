2d337b4b099c583271188740530a0314ed862118
shraavanchess2000 <avinash@uwm.edu>
2021-06-27 21:42:43 -0500

    Message if employee not found

--- InsurancePremiumApp/ClientApp/src/app/employee-premiums/employee-premiums.component.ts
+++ InsurancePremiumApp/ClientApp/src/app/employee-premiums/employee-premiums.component.ts
@@ -7,10 +7,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EmployeePremiumsComponent {
   public employee: Employee;
+  public employeeNotFoundMessage: string;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<Employee>(baseUrl + 'employee/fc84ef57-562d-4073-a334-731b94662a3f').subscribe(result => {
       this.employee = result;
+
+      if (!this.employee) {
+        this.employeeNotFoundMessage = "Employee with given ID was not found.";
+      }
+      else {
+        this.employeeNotFoundMessage = null;
+      }
     }, error => console.error(error));
   }
 }
